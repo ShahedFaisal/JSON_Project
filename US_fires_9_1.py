01e44f0228827c285372014092b1877ad94d39cd
ShahedFaisal <sfaisal2005@gmail.com>
2023-01-03 18:15:23 -0600

    Optimized code

--- US_fires_9_1.py
+++ US_fires_9_1.py
@@ -1,27 +1,32 @@
 import json
 
+#Open and load JSON file
 infile = open('US_fires_9_1.json','r')
-
 fires = json.load(infile)
 
-brights = [fires[index]['brightness'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
-lats = [fires[index]['latitude'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
-lons = [fires[index]['longitude'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
 
+#Get brighness, longitude and lattitude of fires using list comprehension
+brights = [fires[index]['brightness'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
+lons = [fires[index]['longitude'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
+lats = [fires[index]['latitude'] for index,item in enumerate(fires) if fires[index]['brightness'] > 450]
+
+
+#Create offline geoplot to visualize fires
 from plotly.graph_objects import Scattergeo, Layout
 from plotly import offline
 
-data = {'type':'scattergeo',
-        'lon':lons,
-        'lat':lats,
-        'marker':{
-            'size':[15*b/b for b in brights],
-            'color':brights,
-            'colorscale':'Viridis',
-            'reversescale':True,
-            'colorbar':{'title':'Brightness'}
-        }}
-
-layout = Layout(title='US Fires - 9/1/2020 through 9/13/2020')
-fig = {'data':data, 'layout':layout}
+data = {
+    'type':'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'marker':{
+        'size':[15*b/b for b in brights],
+        'color':brights,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Brightness'}
+    }
+}
+mylayout = Layout(title='US Fires: 9/1/2020 through 9/13/2020')
+fig = {'data':data, 'layout':mylayout}
 offline.plot(fig, filename='US_fires_9_1.html')
---
+++ eq_30_days.py
@@ -0,0 +1,40 @@
+import json
+
+#Open and load JSON file
+infile = open('eq_30_days_data.json','r')
+eqdata = json.load(infile)
+
+
+#Convert JSON file to readable dictionary format
+readablefile = open('eq_30_days_data_readable.json','w')
+json.dump(eqdata,readablefile,indent=4)
+readablefile.close()
+
+
+#Get magnitude, longitude, lattitude and place of  earthquakes using list comprehension
+mags = [eq['properties']['mag'] for eq in eqdata['features']]
+lons = [eq['geometry']['coordinates'][0] for eq in eqdata['features']]
+lats = [eq['geometry']['coordinates'][1] for eq in eqdata['features']]
+places = [eq['properties']['place'] for eq in eqdata['features']]
+
+
+#Create offline geoplot to visualize earthquakes
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+
+data = {
+    'type':'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'text':places,
+    'marker':{
+        'size':[5*m for m in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'}
+    }
+}
+mylayout = Layout(title='Global Earthquakes (30 days)')
+fig = {'data':data, 'layout':mylayout}
+offline.plot(fig, filename='eq_30_days_plot.html')
